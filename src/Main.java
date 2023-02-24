f9ab88ac3cf884a0fe9afed8341387623662c954
sujan das <SUJANDAS02CSECUET@GMAIL.COM>
2023-02-24 12:04:23 +0600

    Check the void main method

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("This program will check if the number is digit increasing or not" + "Joy sree Rama");
+    }
+}

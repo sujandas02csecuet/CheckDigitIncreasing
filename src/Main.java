59fa8d7bc1a54e24c6cc56ae86e5b85730ea066e
sujan das <SUJANDAS02CSECUET@GMAIL.COM>
2023-02-24 18:42:45 +0600

    add finalString as a meaningful output.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,29 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("This program will check if the number is digit increasing or not" + "Joy sree Rama");
+        int result = CheckIsDigitIncreasingNumber(37);
+        String finalResult = "the number is not digit increasing";
+        if (result == 1) finalResult = "the number is digit increasing";
+
+        System.out.println(finalResult);
+
+    }
+
+    private static int CheckIsDigitIncreasingNumber(int number) {
+
+
+        for (int i = 1; i <= 9; i++) {
+
+            int sum = 0;
+            int product = 0;
+
+            while (sum < number) {
+
+                product = (product * 10) + i;
+                sum = sum + product;
+            }
+            if (sum == number) return 1;
+        }
+        return 0;
     }
 }
